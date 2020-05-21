0e6c133df51b99bf8fce0ce8bc150849d2fcd3d0
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2020-05-21 16:47:34 -0300

    Fix missing close of if statement from upstream master

--- install/hst-install-ubuntu-wsl.sh
+++ install/hst-install-ubuntu-wsl.sh
@@ -1183,6 +1183,7 @@ if [ "$apache" = 'yes' ]; then
     update-rc.d apache2 defaults > /dev/null 2>&1
     service apache2 start >> $LOG
     check_result $? "apache2 start failed"
+fi
 
 
 #----------------------------------------------------------#
